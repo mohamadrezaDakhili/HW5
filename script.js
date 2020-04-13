4196e4dfbd377568973421aae46dc6e8b44c6d78
mohamadreza dakhili <mohamadrezadakhili53@gmail.com>
2020-04-13 19:12:10 +0430

    finished quiz nine and Done

--- script.js
+++ script.js
@@ -142,3 +142,44 @@ function shuffleArray(array) {
   txtArray.innerHTML ="Array Shuffle = " + "["+array+"]";
 }
 shuffleArray(arr);
+
+//Answer Nine
+function gTOj(gy, gm, gd) {
+  let g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+  let jy = gy <= 1600 ? 0 : 979;
+  gy -= gy <= 1600 ? 621 : 1600;
+  let gy2 = gm > 2 ? gy + 1 : gy;
+  let days =
+    365 * gy +
+    parseInt((gy2 + 3) / 4) -
+    parseInt((gy2 + 99) / 100) +
+    parseInt((gy2 + 399) / 400) -
+    80 +
+    gd +
+    g_d_m[gm - 1];
+  jy += 33 * parseInt(days / 12053);
+  days %= 12053;
+  jy += 4 * parseInt(days / 1461);
+  days %= 1461;
+  jy += parseInt((days - 1) / 365);
+  if (days > 365) days = (days - 1) % 365;
+  let jm =
+    days < 186 ? 1 + parseInt(days / 31) : 7 + parseInt((days - 186) / 30);
+  let jd = 1 + (days < 186 ? days % 31 : (days - 186) % 30);
+  return [jy, jm, jd];
+}
+let dateJalali = document.getElementById("changeDate");
+
+quizNine = { jalali : (stringArg = "0001/01/01") => {
+    const ETF = "۰۱۲۳۴۵۶۷۸۹";
+    let ans = gTOj(...stringArg.split("/").map((elem) => +elem))
+      .map((elem) =>
+        String(elem)
+          .split("")
+          .map((subElem) => ETF[+subElem])
+          .join("")
+      )
+      .join("/");
+    dateJalali.innerHTML = "تاریخ = " + ans;
+  },
+};
